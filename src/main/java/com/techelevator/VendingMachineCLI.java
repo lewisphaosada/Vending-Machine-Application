2324d609f6fc6d82ccde886b6f56d78e27b90021
Greg Westendorf <greg@techelevator.com>
2023-06-01 14:42:07 -0400

    Module 1 capstone base code

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,42 @@
+package com.techelevator;
+
+import com.techelevator.view.Menu;
+
+public class VendingMachineCLI {
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String MAIN_MENU_SECRET_OPTION = "*Sales Report";
+
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_SECRET_OPTION };
+	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+
+
+	private Menu menu;
+
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+	}
+
+	public void run() {
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				// display vending machine items
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				// do purchase
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Menu menu = new Menu(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		cli.run();
+	}
+}
